7cb73b7a576a2ac07656eec2e0d7a06289e21e08
Josef Zabilka <info@midnightfox.eu>
2021-10-15 21:42:59 +0200

    init client

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <!--<div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/app">About</router-link>
+  </div>-->
+  <router-view />
+</template>
+
+<script>
+export default {
+  watch: {
+    $route (to) {document.title = to.meta.title || "White~sender"}
+  },
+  components: {}
+};
+</script>
+<style lang="scss">
+
+</style>
