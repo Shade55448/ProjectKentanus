7dde893392461c1fc199567ef93c8c5f173ed69b
Josef Zabilka <info@midnightfox.eu>
2021-10-15 23:17:58 +0200

    Homepage desktop

---
+++ client/src/components/homepage/HomeForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home-form">
+    <div class="hf-head">
+      <div class="hf-toggle flex-box">
+        <div class="slide-card" id="slideAnim"></div>
+        <button @mouseover="hoveredId(0)" @mouseleave="hoveredId(this.selectId)" class="toggle-item hf-card-active">
+          <span>Sign in</span>
+        </button>
+        <button @mouseover="hoveredId(1)" @mouseleave="hoveredId(this.selectId)" class="toggle-item">
+          <span>Sign up</span>
+        </button>
+      </div>
+    </div>
+    <div class="hf-body">
+      <div class="hf-card hf-card-active">
+        <div class="input-box">
+          <input type="text" name="user" placeholder="Username or email">
+        </div>
+        <div class="input-box">
+          <input type="password" name="user" placeholder="Password">
+        </div>
+        <button class="btn-confirm">Sign in</button>
+      </div>
+      <div class="hf-card">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeForm",
+  components: {},
+  data() {
+    return {
+      hoverId: 0,
+      selectId: 0
+    }
+  },
+  methods: {
+    hoveredId(id){
+      this.hoverId = id;
+      document.getElementById("slideAnim").style.transform = "translateX("+ (id*100) +"%)";
+    },
+    selectedId(id){
+      this.hoverId = id;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -8,16 +8,36 @@
           <img alt="White~sender logo" src="assets/img/logo.svg">
           <span>White~sender</span>
         </router-link>
+        <h1 class="home-title">Welcome to our social network</h1>
+        <p class="home-intro-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod
+          tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad minim
+          veniam, quis nostrud exercitationullamco laboris nisi ut aliquip
+          ex ea commodo consequat.Duis aute irure dolor in reprehenderit in
+          voluptate velitesse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sintoccaecat cupidatat non proident, sunt in culpa
+          qui officiadeserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <span class="home-copyright">Copyright 2021 &copy; Three nerdy dudes</span>
+    </div>
+    <div class="right-side">
+      <div class="home-form-wrap">
+        <HomeForm />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import HomeForm from "@/components/homepage/HomeForm.vue";
+
 export default {
   title: "White~sender",
   name: "Home",
-  components: {},
+  components: {
+    HomeForm
+  },
 };
 </script>
 
