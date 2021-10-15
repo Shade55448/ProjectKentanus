abbfe4402720ca60faa03b70beeb5d837f85b1b4
Jakub Sobotka <speedjakub1@gmail.com>
2021-10-15 20:51:20 +0200

    init server

--- server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http = require("http");
-const port = 80;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n' + res.connection.localPort);
-}).listen(port);
-//# sourceMappingURL=server.js.map
